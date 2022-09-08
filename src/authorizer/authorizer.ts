787ba3eb21facca0f022ed7b3d216f7accbcd6b5
diegosantosouza <diegosantos.s@hotmail.com>
2022-09-08 20:58:22 -0300

    feat: authorize no auth routes

--- src/authorizer/authorizer.ts
+++ src/authorizer/authorizer.ts
@@ -1,9 +1,17 @@
 import type { APIGatewayProxyEvent } from 'aws-lambda';
 
 export const handler = async (event: APIGatewayProxyEvent) => {
-  console.log(event);
-  if (event.headers.authorization === '123') {
+  const { path } = event;
+  const nonAuthenticatedEndpoints = ['/open-route'];
+  /* eslint-disable-next-line */
+  for (const endpoint of nonAuthenticatedEndpoints) {
+    const regex = new RegExp(`^${endpoint}`);
+    if (regex.test(path)) return { isAuthorized: true };
+  }
+
+  if (event.headers.authorization === process.env.AUTHORIZATION_KEY) {
     return { isAuthorized: true };
   }
+
   return { isAuthorized: false };
 };

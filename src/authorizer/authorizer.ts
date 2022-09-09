43d8b0e8968c16c6871fe35e20a646ac4030febf
diegosantosouza <diegosantos.s@hotmail.com>
2022-09-08 21:42:02 -0300

    refactor: remove regex expression

--- src/authorizer/authorizer.ts
+++ src/authorizer/authorizer.ts
@@ -1,14 +1,6 @@
 import type { APIGatewayProxyEvent } from 'aws-lambda';
 
 export const handler = async (event: APIGatewayProxyEvent) => {
-  const { path } = event;
-  const nonAuthenticatedEndpoints = ['/open-route'];
-  /* eslint-disable-next-line */
-  for (const endpoint of nonAuthenticatedEndpoints) {
-    const regex = new RegExp(`^${endpoint}`);
-    if (regex.test(path)) return { isAuthorized: true };
-  }
-
   if (event.headers.authorization === process.env.AUTHORIZATION_KEY) {
     return { isAuthorized: true };
   }

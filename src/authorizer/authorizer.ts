1f342547744dcdc0666eb0842e54ae0e7c8b2b07
diegosantosouza <diegosantos.s@hotmail.com>
2022-09-15 18:30:46 -0300

    feat: add auth scope

--- src/authorizer/authorizer.ts
+++ src/authorizer/authorizer.ts
@@ -1,14 +1,6 @@
-/* eslint-disable no-restricted-syntax */
 import type { APIGatewayProxyEvent } from 'aws-lambda';
 
 export const handler = async (event: APIGatewayProxyEvent) => {
-  const nonAuthenticatedEndpoints = ['/open-route'];
-
-  for (const endpoint of nonAuthenticatedEndpoints) {
-    const regex = new RegExp(`^${endpoint}`);
-    if (regex.test(event.path)) return { isAuthorized: true };
-  }
-
   if (event.headers.authorization === process.env.AUTHORIZATION_KEY) {
     return { isAuthorized: true };
   }
